576c66f88676cbd0c20db8c036a19912c33f603a
aamir0143 <mdaamirreza00@gmail.com>
2022-01-28 03:47:49 +0530

    [Aamir5] Add : Added Flipcoin Problems

---
+++ BasicCorePrograms/FlipCoin/FlipCoin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms.FlipCoin
+{
+    public class FlipCoin
+    {
+        public static void CheckPercentage()
+        {
+            Console.Write("\tEnter the number of times to flip coin: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Random random = new Random();
+            double headCount = 0, tailCount = 0;
+            for (int i = 0; i < num; i++)
+            {
+                double flip = random.Next(0, 2);
+                if (flip < 0.5)
+                {
+                    Console.WriteLine("\tTails");
+                    tailCount++;
+                }
+                else
+                {
+                    Console.WriteLine("\tHeads");
+                    headCount++;
+                }
+            }
+            headCount *= (100 / num);
+            tailCount *= (100 / num);
+            Console.WriteLine("\tThe Percentage of Heads is: {0}% " , headCount);
+            Console.WriteLine("\tThe Percentage of Tails is: {0}% " , tailCount);
+            Console.WriteLine("==================================================================");
+            Console.ReadKey();
+
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,5 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BasicCorePrograms.FlipCoin;
 using System;
-Console.WriteLine("========Welcome To Basic Core Programs====== !");
-Console.ReadLine();
+namespace BasicCoreProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============== Welcome To Basic Core Programs =================");
+            Console.WriteLine();
+            bool flag = true;
+            while (flag)
+            {
+                try
+                {
+                    Console.WriteLine("Choose any one option");
+                    Console.WriteLine("\t1. Flip Coin");
+                    Console.WriteLine("\t2. Exit");
+                    Console.Write("Enter choice: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("\n\t========== 1. Flip Coin==========");
+                            FlipCoin.CheckPercentage();//Call the method
+                            break;
+                        case 2:
+                        default:
+                            flag = false;
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("error message");
+                    flag = false;
+                    
+                }
+            }
+        }
+    }
+}
 
